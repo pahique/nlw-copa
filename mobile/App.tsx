ce78cd86abf6fa9f46636f6aa4070fbcd5cd07be
Paulo Oliveros <V8T2Z6L@johndeere.com>
2022-11-06 00:35:41 -0300

    status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
-        <Text color="white" fontSize={24}>Hello React Native!</Text>
-        <StatusBar style="auto" />
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <SignIn/> : <Loading/> }
     </NativeBaseProvider>
   );
 }
